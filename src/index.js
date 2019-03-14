72ba59137e435bdd00aa9ce773c0369b95c341f6
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-03-14 14:05:47 -0300

    desafio completo

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+import './styles/app.scss';
+
+render(<App />, document.getElementById('app'));
